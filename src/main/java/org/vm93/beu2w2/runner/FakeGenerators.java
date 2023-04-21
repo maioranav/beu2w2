d88bae1b8c97e7d611cf2db9e2fb5824688b19c4
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 12:37:57 +0200

    Create FakeGenerators.java

---
+++ src/main/java/org/vm93/beu2w2/runner/FakeGenerators.java
@@ -0,0 +1,36 @@
+package org.vm93.beu2w2.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.vm93.beu2w2.service.EmployeeService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class FakeGenerators implements ApplicationRunner {
+	
+	@Autowired
+	EmployeeService employeedao;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		//Uncomment this line to make generator start on Boot
+		generateFakes();
+
+	}
+	
+	public void generateFakes() {
+		log.warn("FakeGeneratorStarted");
+		
+		for (int i = 0; i < 10; i++) {
+		employeedao.saveFakeEmployee();
+		}
+		
+		log.warn("FakeGeneratorEnded");
+	}
+
+}
