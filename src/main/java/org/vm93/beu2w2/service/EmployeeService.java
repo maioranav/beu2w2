fa6f32e16083d6187c6c31c7170575ca7f217a81
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 12:31:17 +0200

    created CRUD services Employee

--- src/main/java/org/vm93/beu2w2/service/EmployeeService.java
+++ src/main/java/org/vm93/beu2w2/service/EmployeeService.java
@@ -3,11 +3,14 @@ package org.vm93.beu2w2.service;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.vm93.beu2w2.model.Employee;
 import org.vm93.beu2w2.repo.EmployeeDaoRepo;
 
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EmployeeService {
@@ -29,4 +32,44 @@ public class EmployeeService {
 		saveEmployee(e);
 	}
 	
+	public String removeEmployee(Long id) {
+		if (!repo.existsById(id)) {
+			throw new EntityNotFoundException("Employee ID doesnt exists!!");
+		}
+		repo.deleteById(id);
+		return "Employee Deleted from DB!";
+	}
+	
+	public Employee updateEmployee(Employee e) {
+		if (!repo.existsById(e.getId())) {
+			throw new EntityNotFoundException("Employee ID doesnt exists on No Id Specified!!");
+		}
+		repo.save(e);
+		return e;
+	}
+	
+	public Employee findById(Long id) {
+		if(!repo.existsById(id)) {
+			throw new EntityNotFoundException("Employee ID doesnt exists!!");
+		}
+		return repo.findById(id).get();	
+	}
+	
+	public Employee findByEmail(String email) {
+		if(!repo.existsByEmail(email)) {
+			throw new EntityNotFoundException("Employee Email doesnt exists!!");
+		}
+		return repo.findByEmail(email).get();	
+	}
+	
+	public Employee findByUsername(String username) {
+		if(!repo.existsByUsername(username)) {
+			throw new EntityNotFoundException("Employee Username doesnt exists!!");
+		}
+		return repo.findByUsername(username).get();	
+	}
+	
+	public Page<Employee> findAll(Pageable pageable){
+		return (Page<Employee>) repo.findAll(pageable);
+	}
 }
