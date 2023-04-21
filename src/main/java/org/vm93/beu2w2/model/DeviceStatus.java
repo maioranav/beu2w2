daddb47ce570ad150b80f09e2089d634a69b7d03
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 11:59:31 +0200

    added random method for enums

--- src/main/java/org/vm93/beu2w2/model/DeviceStatus.java
+++ src/main/java/org/vm93/beu2w2/model/DeviceStatus.java
@@ -1,5 +1,14 @@
 package org.vm93.beu2w2.model;
 
+import java.util.Random;
+
 public enum DeviceStatus {
-AVAILABLE, ASSIGNED, MAINTENANCE, OUTOFORDER
+AVAILABLE, ASSIGNED, MAINTENANCE, OUTOFORDER;
+	
+	private static final Random rand = new Random();
+
+    public static DeviceStatus randomDeviceStatus()  {
+    	DeviceStatus[] statuses = values();
+        return statuses[rand.nextInt(statuses.length)];
+    }
 }
--- src/main/java/org/vm93/beu2w2/model/DeviceType.java
+++ src/main/java/org/vm93/beu2w2/model/DeviceType.java
@@ -1,5 +1,14 @@
 package org.vm93.beu2w2.model;
 
+import java.util.Random;
+
 public enum DeviceType {
-SMARTPHONE, TABLET, LAPTOP
+SMARTPHONE, TABLET, LAPTOP;
+
+private static final Random rand = new Random();
+
+public static DeviceType randomDeviceStatus()  {
+	DeviceType[] types = values();
+    return types[rand.nextInt(types.length)];
+}
 }
