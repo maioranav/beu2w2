e0e5f759305401af15574d50e86d19c8fc4a73a7
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 12:16:55 +0200

    impl find/exist employee by email/user

--- src/main/java/org/vm93/beu2w2/repo/EmployeeDaoRepo.java
+++ src/main/java/org/vm93/beu2w2/repo/EmployeeDaoRepo.java
@@ -1,9 +1,16 @@
 package org.vm93.beu2w2.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.vm93.beu2w2.model.Employee;
 
 public interface EmployeeDaoRepo extends CrudRepository<Employee, Long>, PagingAndSortingRepository<Employee, Long> {
 
+	boolean existsByEmail(String email);
+	boolean existsByUsername(String username);
+	Optional<Employee> findByEmail(String email);
+	Optional<Employee> findByUsername(String username);
+	
 }
