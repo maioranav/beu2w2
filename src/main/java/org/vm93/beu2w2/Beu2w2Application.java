abc256e7fa202800ff12a764cb86e3ea2805b196
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 11:31:20 +0200

    created springprj with dependencies

---
+++ src/main/java/org/vm93/beu2w2/Beu2w2Application.java
@@ -0,0 +1,13 @@
+package org.vm93.beu2w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu2w2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu2w2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/vm93/beu2w2/Beu2w2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vm93.beu2w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beu2w2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
