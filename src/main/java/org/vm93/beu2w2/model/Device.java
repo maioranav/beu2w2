020ec75030d41de9f04268fc33064a5125c5cb8b
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 11:41:59 +0200

    created entities with relations

---
+++ src/main/java/org/vm93/beu2w2/model/Device.java
@@ -0,0 +1,36 @@
+package org.vm93.beu2w2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "devices")
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Device {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	private DeviceType type;
+
+	@Enumerated(EnumType.STRING)
+	private DeviceStatus status;
+	
+	@ManyToOne
+	private Employee employee;
+
+}
---
+++ src/main/java/org/vm93/beu2w2/model/Employee.java
@@ -0,0 +1,48 @@
+package org.vm93.beu2w2.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String firsname;
+
+	@Column(nullable = false)
+	private String lastname;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@OneToMany(mappedBy = "employee")
+	private List<Device> devices;
+}
