f8c671ca3df683681629a432b1c069c91f78a44c
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-21 12:31:45 +0200

    minorfix

--- src/main/java/org/vm93/beu2w2/model/Employee.java
+++ src/main/java/org/vm93/beu2w2/model/Employee.java
@@ -2,17 +2,11 @@ package org.vm93.beu2w2.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
